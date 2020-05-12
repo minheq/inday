91005fba61dbf3a5021d8146fa5fc572f55a1d8e
Vu Hoang Minh <vuhminh@gmail.com>
2020-05-12 22:40:28 +0700

    slate

--- app/editor/editor.tsx
+++ app/editor/editor.tsx
@@ -1,69 +1,144 @@
-import Delta from 'quill-delta';
 import React from 'react';
-// import { EditorMobile } from './editor_mobile';
-import { EditorDesktop } from './editor_desktop';
+import isHotkey from 'is-hotkey';
+import {
+  Editable,
+  withReact,
+  Slate,
+  RenderElementProps,
+  RenderLeafProps,
+} from 'slate-react';
+import { Editor as SlateEditor, createEditor, Node } from 'slate';
+import { withHistory } from 'slate-history';
 
-// Type of commands
-// Inline format
-//  - Emphasis (Bold, Italic, Code, Strike)
-//  - Link
-// Block format
-//  - Headers
-//  - Lists
-//  - Blockquotes
-//  - Code Block
-// Blocks
-//  - Images
-//  - Divider
-//  - Videos
-//  - Tables
+const HOTKEYS: { [key: string]: string } = {
+  'mod+b': 'bold',
+  'mod+i': 'italic',
+  'mod+u': 'underline',
+  'mod+`': 'code',
+};
 
-// Desktop
-// HoverableToolbar -> Inline format + Block format
-// Commands -> Block format + Blocks
+export function Editor() {
+  const [value, setValue] = React.useState<Node[]>(initialValue);
+  const renderElement = React.useCallback(
+    (props) => <Element {...props} />,
+    [],
+  );
+  const renderLeaf = React.useCallback((props) => <Leaf {...props} />, []);
+  const editor = React.useMemo(
+    () => withHistory(withReact(createEditor())),
+    [],
+  );
 
-// Mobile
-// Selection -> Inline Format
-// Collapsed selection -> Block format + Blocks
-// Collapsed on link -> Link edit/preview
-
-// TODO:
-// Don't use iframe
-// - Markdown auto formatting
-// - ``` code block
-// - --- horizontal rule
-// - Code and Code block syntax highlighting
-// - Proper icons
-// - Disable pasted formats
-// - Paste image
-
-// - Smooth loading of editor
-// - Drag and drop image/video into editor
-// - Embed Videos
-// - Embed Tweets
-// - Embed Drawings
-
-export interface EditorProps {
-  initialContent?: Delta;
+  return (
+    <Slate editor={editor} value={value} onChange={setValue}>
+      <Editable
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        placeholder="Enter some rich text…"
+        spellCheck
+        autoFocus
+        onKeyDown={(event) => {
+          for (const hotkey in HOTKEYS) {
+            // @ts-ignore
+            if (isHotkey(hotkey, event)) {
+              event.preventDefault();
+              const mark = HOTKEYS[hotkey];
+              toggleMark(editor, mark);
+            }
+          }
+        }}
+      />
+    </Slate>
+  );
 }
 
-export function Editor(props: EditorProps) {
-  const {
-    initialContent = new Delta()
-      .insert(
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet nulla tortor, ut consequat metus imperdiet eu. Aenean viverra non mi convallis auctor. Nullam felis elit, varius ut maximus sed, luctus ac arcu. Sed tincidunt, nibh eget ultrices tincidunt, felis eros commodo felis, vel ornare nibh sapien vel metus. Vivamus eu tristique sapien. Pellentesque imperdiet porttitor velit at pharetra. Morbi sem orci, dictum id sapien vel, ullamcorper semper neque.\n\n',
-      )
-      // .insert(
-      //   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet nulla tortor, ut consequat metus imperdiet eu. Aenean viverra non mi convallis auctor. Nullam felis elit, varius ut maximus sed, luctus ac arcu. Sed tincidunt, nibh eget ultrices tincidunt, felis eros commodo felis, vel ornare nibh sapien vel metus. Vivamus eu tristique sapien. Pellentesque imperdiet porttitor velit at pharetra. Morbi sem orci, dictum id sapien vel, ullamcorper semper neque.\n\n',
-      // )
-      // .insert(
-      //   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet nulla tortor, ut consequat metus imperdiet eu. Aenean viverra non mi convallis auctor. Nullam felis elit, varius ut maximus sed, luctus ac arcu. Sed tincidunt, nibh eget ultrices tincidunt, felis eros commodo felis, vel ornare nibh sapien vel metus. Vivamus eu tristique sapien. Pellentesque imperdiet porttitor velit at pharetra. Morbi sem orci, dictum id sapien vel, ullamcorper semper neque.\n',
-      // )
-      // .insert(
-      //   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet nulla tortor, ut consequat metus imperdiet eu. Aenean viverra non mi convallis auctor. Nullam felis elit, varius ut maximus sed, luctus ac arcu. Sed tincidunt, nibh eget ultrices tincidunt, felis eros commodo felis, vel ornare nibh sapien vel metus. Vivamus eu tristique sapien. Pellentesque imperdiet porttitor velit at pharetra. Morbi sem orci, dictum id sapien vel, ullamcorper semper neque.\n',
-      // )
-      .insert('\n'),
-  } = props;
+const isMarkActive = (editor: SlateEditor, format: string) => {
+  const marks = SlateEditor.marks(editor);
+  return marks ? marks[format] === true : false;
+};
 
-  return <EditorDesktop initialContent={initialContent} />;
+const toggleMark = (editor: SlateEditor, format: string) => {
+  const isActive = isMarkActive(editor, format);
+
+  if (isActive) {
+    SlateEditor.removeMark(editor, format);
+  } else {
+    SlateEditor.addMark(editor, format, true);
+  }
+};
+
+const Element = ({ attributes, children, element }: RenderElementProps) => {
+  switch (element.type) {
+    case 'block-quote':
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case 'bulleted-list':
+      return <ul {...attributes}>{children}</ul>;
+    case 'heading-one':
+      return <h1 {...attributes}>{children}</h1>;
+    case 'heading-two':
+      return <h2 {...attributes}>{children}</h2>;
+    case 'list-item':
+      return <li {...attributes}>{children}</li>;
+    case 'numbered-list':
+      return <ol {...attributes}>{children}</ol>;
+    default:
+      return <p {...attributes}>{children}</p>;
+  }
+};
+
+function Leaf({ attributes, children, leaf }: RenderLeafProps) {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>;
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>;
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>;
+  }
+
+  if (leaf.underline) {
+    children = <u>{children}</u>;
+  }
+
+  return <span {...attributes}>{children}</span>;
 }
+
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [
+      { text: 'This is editable ' },
+      { text: 'rich', bold: true },
+      { text: ' text, ' },
+      { text: 'much', italic: true },
+      { text: ' better than a ' },
+      { text: '<textarea>', code: true },
+      { text: '!' },
+    ],
+  },
+  {
+    type: 'paragraph',
+    children: [
+      {
+        text:
+          "Since it's rich text, you can do things like turn a selection of text ",
+      },
+      { text: 'bold', bold: true },
+      {
+        text:
+          ', or add a semantically rendered block quote in the middle of the page, like this:',
+      },
+    ],
+  },
+  {
+    type: 'block-quote',
+    children: [{ text: 'A wise quote.' }],
+  },
+  {
+    type: 'paragraph',
+    children: [{ text: 'Try it out for yourself!' }],
+  },
+];
