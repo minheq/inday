5299f697e87d93981c09e27a4f3c64b5653aae32
Vu Hoang Minh <vuhminh@gmail.com>
2020-05-22 16:01:25 +0700

    hotkeys

--- app/editor/editor.tsx
+++ app/editor/editor.tsx
@@ -7,14 +7,69 @@ import { Leaf, Mark } from './nodes/leaf';
 import { withShortcuts } from './plugins/shortcuts';
 import { withChecklists } from './plugins/checklists';
 import { withLinks } from './plugins/links';
-import { Element, BlockType } from './element';
+import { Element, BlockType, ElementType, InlineType } from './element';
 
-const HOTKEYS: { [key: string]: Mark } = {
+const HOTKEYS: { [key: string]: Mark | ElementType } = {
   'mod+b': 'bold',
   'mod+i': 'italic',
   'mod+shift+c': 'code',
   'mod+shift+x': 'strikethrough',
+  'mod+shift+7': 'numbered-list',
+  'mod+shift+8': 'bulleted-list',
+  'mod+shift+9': 'block-quote',
+  'mod+shift+alt+c': 'code-block',
+  'mod+shift+u': 'link',
 };
+
+const MARKS: { [key: string]: true } = {
+  bold: true,
+  italic: true,
+  code: true,
+  strikethrough: true,
+};
+
+function isMark(format: string): format is Mark {
+  if (MARKS[format]) {
+    return true;
+  }
+
+  return false;
+}
+
+const BLOCKS: { [key: string]: true } = {
+  paragraph: true,
+  'bulleted-list': true,
+  'numbered-list': true,
+  'list-item': true,
+  'block-quote': true,
+  'code-block': true,
+  divider: true,
+  'check-list-item': true,
+  'heading-one': true,
+  'heading-two': true,
+  'heading-three': true,
+  image: true,
+};
+
+function isBlock(format: string): format is BlockType {
+  if (BLOCKS[format]) {
+    return true;
+  }
+
+  return false;
+}
+
+const INLINES: { [key: string]: true } = {
+  link: true,
+};
+
+function isInline(format: string): format is InlineType {
+  if (INLINES[format]) {
+    return true;
+  }
+
+  return false;
+}
 
 // TODO:
 // - ~~Divider~~
@@ -32,10 +87,10 @@ const HOTKEYS: { [key: string]: Mark } = {
 //  - ~~Italic~~
 //  - ~~Code~~
 //  - ~~Strikethrough~~
-//  - Numbered list
-//  - Bulleted list
-//  - Block quote
-//  - Code block
+//  - ~~Numbered list~~
+//  - ~~Bulleted list~~
+//  - ~~Block quote~~
+//  - ~~Code block~~
 //  - Link
 // - Formatting Toolbar
 // - Link Toolbar
@@ -68,8 +123,17 @@ export function MyEditor(props: MyEditorProps) {
       for (const hotkey in HOTKEYS) {
         if (isHotkey(hotkey, event)) {
           event.preventDefault();
-          const mark = HOTKEYS[hotkey];
-          toggleMark(editor, mark);
+          const format = HOTKEYS[hotkey];
+
+          if (isMark(format)) {
+            toggleMark(editor, format);
+          } else if (isBlock(format)) {
+            toggleBlock(editor, format);
+          } else if (isInline(format)) {
+            if (format === 'link') {
+              // TODO
+            }
+          }
         }
       }
     },
