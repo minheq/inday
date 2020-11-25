45358743df870abd7c9eb1c55cc6255d9979d2c2
Vu Hoang Minh <vuhminh@gmail.com>
2020-11-26 01:40:24 +0700

    liver reload

--- scripts/dev_web.js
+++ scripts/dev_web.js
@@ -9,40 +9,48 @@ const STATIC_SERVER_PORT = 8080;
 
 let html = '';
 
-const server = http.createServer((_req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.writeHead(200);
-  res.end(html);
-});
-
 fs.readFile(path.resolve(__dirname, '../index.html'), (err, contents) => {
   if (err !== null) {
-    console.error(`Could not read index.html file: ${err.message}`);
     process.exit(1);
   }
 
-  html = contents
-    .toString()
-    .replace('/public', `http://localhost:${BUILD_SERVER_PORT}`);
+  html = contents.toString();
+  html = html.replace('/public', `http://localhost:${BUILD_SERVER_PORT}`);
+  html = html.replace(
+    '<div id="dev"></div>',
+    `<script>fetch('http://localhost:${STATIC_SERVER_PORT}/check').then(() => window.location.reload())</script>`,
+  );
 
-  startStaticServer();
-  startBuildServer();
-});
-
-function startStaticServer() {
-  server.listen(STATIC_SERVER_PORT, 'localhost', () => {
-    console.log(
-      `Static server is running on http://localhost:${STATIC_SERVER_PORT}`,
-    );
-  });
-}
-
-function startBuildServer() {
   serve({ port: BUILD_SERVER_PORT }, options)
     .then(() => {
       console.log(
         `Build server is running on http://localhost:${BUILD_SERVER_PORT}`,
       );
+
+      const server = http.createServer((req, res) => {
+        if (req.method === 'GET' && req.url === '/check') {
+          const watcher = fs.watch(
+            path.resolve(__dirname, '../app'),
+            { recursive: true },
+            () => {
+              watcher.close();
+              res.writeHead(200);
+              res.end();
+            },
+          );
+          return;
+        }
+
+        res.setHeader('Content-Type', 'text/html');
+        res.writeHead(200);
+        res.end(html);
+      });
+
+      server.listen(STATIC_SERVER_PORT, 'localhost', () => {
+        console.log(
+          `Static server is running on http://localhost:${STATIC_SERVER_PORT}`,
+        );
+      });
     })
     .catch(() => process.exit(1));
-}
+});
