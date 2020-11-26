6a37a980749172fb1aea62e27801f8be19ae2cbe
Vu Hoang Minh <vuhminh@gmail.com>
2020-11-26 19:07:17 +0700

    revert dev script

--- scripts/dev_web.js
+++ scripts/dev_web.js
@@ -8,7 +8,6 @@ const BUILD_SERVER_PORT = 8000;
 const STATIC_SERVER_PORT = 8080;
 
 let html = '';
-let watcher = null;
 
 fs.readFile(path.resolve(__dirname, '../index.html'), (err, contents) => {
   if (err !== null) {
@@ -17,42 +16,14 @@ fs.readFile(path.resolve(__dirname, '../index.html'), (err, contents) => {
 
   html = contents.toString();
   html = html.replace('/public', `http://localhost:${BUILD_SERVER_PORT}`);
-  html = html.replace(
-    '<div id="dev"></div>',
-    `<script>fetch('http://localhost:${STATIC_SERVER_PORT}/check').then(() => window.location.reload())</script>`,
-  );
 
-  serve(
-    {
-      port: BUILD_SERVER_PORT,
-      onRequest: () => {
-        if (watcher !== null) {
-          watcher.close();
-          watcher = null;
-        }
-      },
-    },
-    options,
-  )
+  serve({ port: BUILD_SERVER_PORT }, options)
     .then(() => {
       console.log(
         `Build server is running on http://localhost:${BUILD_SERVER_PORT}`,
       );
 
       const server = http.createServer((req, res) => {
-        if (req.method === 'GET' && req.url === '/check') {
-          watcher = fs.watch(
-            path.resolve(__dirname, '../app'),
-            { recursive: true },
-            () => {
-              watcher.close();
-              res.writeHead(200);
-              res.end();
-            },
-          );
-          return;
-        }
-
         res.setHeader('Content-Type', 'text/html');
         res.writeHead(200);
         res.end(html);
@@ -64,5 +35,8 @@ fs.readFile(path.resolve(__dirname, '../index.html'), (err, contents) => {
         );
       });
     })
-    .catch(() => process.exit(1));
+    .catch((error) => {
+      console.error(error.message);
+      process.exit(1);
+    });
 });
