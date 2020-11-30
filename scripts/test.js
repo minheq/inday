c0ab0e96c00ea7a5baa46e29f3c29ffbab1977f9
Vu Hoang Minh <vuhminh@gmail.com>
2020-11-30 17:50:10 +0700

    test script

--- scripts/test.js
+++ scripts/test.js
@@ -6,27 +6,73 @@ const Parser = require('tap-parser');
 const { mapConcurrent } = require('./utils');
 
 const numCPUs = os.cpus().length;
-const testFile = process.argv[2];
 
 async function main() {
+  const start = new Date();
   const testFiles = await glob('test/**/*.test.js');
-
-  console.time('measure');
   const results = await mapConcurrent(testFiles, test, {
     concurrency: numCPUs,
   });
-  console.log(results, 'results');
-  console.timeEnd('measure');
+
+  let failed = false;
+
+  for (const result of results) {
+    if (result.ok) {
+      console.log(
+        '\x1b[42m\x1b[30m',
+        'PASS',
+        '\x1b[0m',
+        result.file,
+        `\x1b[2m${result.count}`,
+        `\x1b[0m\x1b[33m${result.time}ms`,
+      );
+    } else {
+      failed = true;
+      console.log(
+        '\x1b[41m\x1b[30m',
+        'FAIL',
+        '\x1b[0m',
+        result.file,
+        `\x1b[31m${result.fail} failed`,
+        `\x1b[0m\x1b[2mof ${result.count}`,
+        `\x1b[0m\x1b[33m${result.time}ms`,
+      );
+
+      for (const failure of result.failures) {
+        console.log(`\x1b[0m\x1b[31m${failure.id} ${failure.name}`);
+        console.log(
+          `\x1b[0mexpected\n${JSON.stringify(
+            failure.diag.expected,
+            null,
+            2,
+          )}\n`,
+        );
+        console.log(`got\n${JSON.stringify(failure.diag.actual, null, 2)}`);
+        console.log('\n');
+        console.log('\n');
+      }
+    }
+  }
+
+  const end = new Date() - start;
+
+  console.log(`\x1b[0mDone in \x1b[33m${end}ms`);
+  if (failed) {
+    throw new Error('');
+  }
 }
 
 function test(file) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
+    const start = new Date();
+
     const p = new Parser((results) => {
-      if (results.ok) {
-        resolve(file + 'passed');
-      } else {
-        reject(file + 'failed');
-      }
+      const end = new Date() - start;
+      resolve({
+        ...results,
+        file,
+        time: end,
+      });
     });
 
     const child = child_process.fork(file, { stdio: 'pipe' });
