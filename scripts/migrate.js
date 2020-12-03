3ad1eaf1e16efc7545fa4755878a792ac7bb2e70
Vu Hoang Minh <vuhminh@gmail.com>
2020-12-03 10:00:03 +0700

    add back

---
+++ scripts/migrate.js
@@ -0,0 +1,11 @@
+const Postgrator = require('postgrator');
+
+const postgrator = new Postgrator({
+  driver: 'pg',
+  connectionString: process.env.DATABASE_URL,
+});
+
+postgrator
+  .migrate()
+  .then((appliedMigrations) => console.log(appliedMigrations))
+  .catch((error) => console.log(error));
