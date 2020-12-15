f8438fbda3552b1e80fc3f1979bf71dd9b51606e
Vu Hoang Minh <vuhminh@gmail.com>
2020-12-15 15:19:50 +0700

    add hoverable

---
+++ app/components/hoverable.tsx
@@ -0,0 +1,98 @@
+import React, { useCallback, useState } from 'react';
+import { canUseDOM } from '../lib/execution_environment';
+
+interface HoverableProps {
+  children: React.ReactElement | ((hovered: boolean) => React.ReactElement);
+  onHoverIn?: () => void;
+  onHoverOut?: () => void;
+}
+
+export function Hoverable(props: HoverableProps): JSX.Element {
+  const { children, onHoverIn, onHoverOut } = props;
+  const [state, setState] = useState({ hovered: false, showHover: true });
+  const { hovered, showHover } = state;
+
+  const handleMouseEnter = useCallback(() => {
+    if (isHoverEnabled() && !hovered) {
+      if (onHoverIn !== undefined) {
+        onHoverIn();
+      }
+
+      setState({
+        showHover,
+        hovered: true,
+      });
+    }
+  }, [showHover, hovered, onHoverIn]);
+
+  const handleMouseLeave = useCallback(() => {
+    if (hovered) {
+      if (onHoverOut !== undefined) {
+        onHoverOut();
+      }
+      setState({
+        showHover,
+        hovered: false,
+      });
+    }
+  }, [hovered, showHover, onHoverOut]);
+
+  const handleGrant = useCallback(() => {
+    setState({ showHover: false, hovered });
+  }, [hovered]);
+
+  const handleRelease = useCallback(() => {
+    setState({ showHover: true, hovered });
+  }, [hovered]);
+
+  const child =
+    typeof children === 'function' ? children(showHover && hovered) : children;
+
+  return React.cloneElement(React.Children.only(child), {
+    onMouseEnter: handleMouseEnter,
+    onMouseLeave: handleMouseLeave,
+    // prevent hover showing while responder
+    onResponderGrant: handleGrant,
+    onResponderRelease: handleRelease,
+    // if child is Pressable
+    onPressIn: handleGrant,
+    onPressOut: handleRelease,
+  });
+}
+
+let isEnabled = false;
+
+/**
+ * Web browsers emulate mouse events (and hover states) after touch events.
+ * This code infers when the currently-in-use modality supports hover
+ * (including for multi-modality devices) and considers "hover" to be enabled
+ * if a mouse movement occurs more than 1 second after the last touch event.
+ * This threshold is long enough to account for longer delays between the
+ * browser firing touch and mouse events on low-powered devices.
+ */
+const HOVER_THRESHOLD_MS = 1000;
+let lastTouchTimestamp = 0;
+
+function enableHover() {
+  if (isEnabled || Date.now() - lastTouchTimestamp < HOVER_THRESHOLD_MS) {
+    return;
+  }
+  isEnabled = true;
+}
+
+function disableHover() {
+  lastTouchTimestamp = Date.now();
+  if (isEnabled) {
+    isEnabled = false;
+  }
+}
+
+if (canUseDOM) {
+  document.addEventListener('touchstart', disableHover, true);
+  document.addEventListener('touchmove', disableHover, true);
+  document.addEventListener('mousemove', enableHover, true);
+}
+
+function isHoverEnabled(): boolean {
+  return isEnabled;
+}
