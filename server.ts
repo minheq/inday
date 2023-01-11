618b958524cd76a08e69039d8fac45a23d02de36
Vu Hoang Minh <vuhminh@gmail.com>
2023-01-11 09:57:14 +0700

    remove server

--- server.ts
+++
@@ -1,8 +0,0 @@
-import { env } from "./api/env";
-import { createAPI } from "./api/api";
-
-const api = createAPI();
-
-api.listen(env.port, () =>
-  console.log(`Server listening at http://localhost:${env.port}`)
-);
